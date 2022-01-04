beaa476d22bc68061afc1dd18b60157fa3a3f4b6
nguyenminhdungtd98 <nguyenminhdungtd98@gmail.com>
2022-01-04 23:47:33 +0700

    update relationship and edit entity

--- src/main/java/com/comaymanagement/cmd/controller/DepartmentController.java
+++ src/main/java/com/comaymanagement/cmd/controller/DepartmentController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.comaymanagement.cmd.constant.CrossOriginConstant;
 import com.comaymanagement.cmd.customentity.CustomDepartmentAll;
 import com.comaymanagement.cmd.entity.Department;
 import com.comaymanagement.cmd.entity.ResponseObject;
@@ -17,6 +19,7 @@ import com.comaymanagement.cmd.service.DepartmentService;
 
 @RestController
 @RequestMapping("/departments")
+@CrossOrigin(origins = CrossOriginConstant.REACT_ORIGIN)
 public class DepartmentController {
 	@Autowired
 	DepartmentService departmentService;
--- src/main/java/com/comaymanagement/cmd/controller/PositionController.java
+++ src/main/java/com/comaymanagement/cmd/controller/PositionController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.comaymanagement.cmd.constant.CrossOriginConstant;
 import com.comaymanagement.cmd.customentity.CustomPositionAll;
 import com.comaymanagement.cmd.entity.Position;
 import com.comaymanagement.cmd.entity.ResponseObject;
@@ -17,6 +19,7 @@ import com.comaymanagement.cmd.service.PositionService;
 
 @RestController
 @RequestMapping("/positions")
+@CrossOrigin(origins = CrossOriginConstant.REACT_ORIGIN)
 public class PositionController {
 	@Autowired
 	PositionService positionService;
