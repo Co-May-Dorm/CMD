2b960bf19050094a178abef13d04e87a59d46b73
tnguyen669 <toanvocong.nguyen@dxc.com>
2022-01-17 23:14:07 +0700

    CMD-45 Create API get employee list

--- src/main/java/com/comaymanagement/cmd/service/EmployeeService.java
+++ src/main/java/com/comaymanagement/cmd/service/EmployeeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import com.comaymanagement.cmd.entity.Employee;
@@ -39,4 +40,9 @@ public class EmployeeService implements IGeneralService<Employee> {
 	public List<Employee> findByActiveFlag(Boolean flag) {
 		return employeeRepository.findByActiveFlag(flag);
 	}
+	
+	public List<Employee> employeePaging(String name, String dob, String email, String phone, String dep, String pos, String sort, String order, Integer limit, Integer offset) {
+		return employeeRepository.employeePaging(name, dob, email, phone, dep, pos, sort, order, limit, offset);
+	}
+
 }
