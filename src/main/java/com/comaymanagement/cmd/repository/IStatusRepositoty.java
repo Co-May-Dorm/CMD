5bcfe97e9f2e2755911d5939957ad022b6be92c9
dnguyen <dxc>
2022-07-09 00:44:41 +0700

    Ticket #CMD-49 Create API insert task &&  CMD-66 Create API edit task

--- src/main/java/com/comaymanagement/cmd/repository/IStatusRepositoty.java
+++ src/main/java/com/comaymanagement/cmd/repository/IStatusRepositoty.java
@@ -2,7 +2,9 @@ package com.comaymanagement.cmd.repository;
 
 import org.springframework.stereotype.Repository;
 
+import com.comaymanagement.cmd.entity.Status;
+
 @Repository
 public interface IStatusRepositoty {
-
+	Status findById(Integer id);
 }
--- src/main/java/com/comaymanagement/cmd/repositoryimpl/StatusRepositotyImpl.java
+++ src/main/java/com/comaymanagement/cmd/repositoryimpl/StatusRepositotyImpl.java
@@ -1,9 +1,54 @@
 package com.comaymanagement.cmd.repositoryimpl;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.comaymanagement.cmd.entity.Department;
+import com.comaymanagement.cmd.entity.Status;
+import com.comaymanagement.cmd.entity.Task;
+import com.comaymanagement.cmd.model.DepartmentModel;
+import com.comaymanagement.cmd.model.EmployeeModel;
+import com.comaymanagement.cmd.model.TaskModel;
 import com.comaymanagement.cmd.repository.IStatusRepositoty;
+
+import net.bytebuddy.asm.Advice.This;
 @Repository
 public class StatusRepositotyImpl implements IStatusRepositoty {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(This.class);
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	public Status findById(Integer id) {
+		Status status = new Status();
+		StringBuilder hql = new StringBuilder("FROM statuses AS st ");
+		hql.append("WHERE st.id = :id");
+		try {
+			Session session = sessionFactory.getCurrentSession();
+			Query query = session.createQuery(hql.toString());
+			LOGGER.info(hql.toString());
+			query.setParameter("id",id);			
+			for (Iterator it = query.getResultList().iterator(); it.hasNext();) {
+				status = (Status) it.next();
+			}
+
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
+		return status;
+	}
 
 }
