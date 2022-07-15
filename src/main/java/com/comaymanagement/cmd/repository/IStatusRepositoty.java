ec370b7894e3633174effec8d6b56b1fb7041e6f
dnguyen <dxc>
2022-07-15 18:09:00 +0700

    ticket #CMD-87 Find All Status

--- src/main/java/com/comaymanagement/cmd/repository/IStatusRepositoty.java
+++ src/main/java/com/comaymanagement/cmd/repository/IStatusRepositoty.java
@@ -1,5 +1,7 @@
 package com.comaymanagement.cmd.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.comaymanagement.cmd.entity.Status;
@@ -7,4 +9,5 @@ import com.comaymanagement.cmd.entity.Status;
 @Repository
 public interface IStatusRepositoty {
 	Status findById(Integer id);
+	List<Status> findAll();
 }
