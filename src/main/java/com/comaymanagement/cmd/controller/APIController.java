6dedf98c89f4631d6a1238d780e4d6ea6f709bf5
tnguyen669 <toanvocong.nguyen@dxc.com>
2022-07-23 20:58:35 +0700

    [API] Upload image post

--- src/main/java/com/comaymanagement/cmd/controller/APIController.java
+++ src/main/java/com/comaymanagement/cmd/controller/APIController.java
@@ -1,14 +1,26 @@
 package com.comaymanagement.cmd.controller;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.poi.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,41 +29,57 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.comaymanagement.cmd.constant.CrossOriginConstant;
+import com.comaymanagement.cmd.entity.ResponseObject;
 
 @RestController
-@RequestMapping("/API")
+@RequestMapping("/api")
 @CrossOrigin(origins = CrossOriginConstant.REACT_ORIGIN)
 public class APIController {
 	@Autowired
 	ServletContext context;
-	@PostMapping("/UploadFile")
+	@PostMapping("/upload-images")
 	@ResponseBody
-	public String uploadFile(MultipartHttpServletRequest request) {
+	public ResponseEntity<Object> uploadFile(MultipartHttpServletRequest request) {
 //		// Lay r ds ten file
 			MultiValueMap<String, MultipartFile>  form = request.getMultiFileMap();
-			
-			List<MultipartFile> files = form.get("file");
-			String pathSaveFile = "";
+			List<MultipartFile> files = form.get("image");
+			StringBuilder pathSaveFile = new StringBuilder(System.getProperty("user.dir"));
 			for(MultipartFile mpf : files) {
 				if( mpf.getOriginalFilename().equals("")) {
 					continue;
 				}
-//			B1: lay ra duong dan se luu file
-//				C:\Practive\Source\CMD-FE\build
-				pathSaveFile = "C:\\Practive\\Source\\CMD-FE\\build\\";
-//			B2: Tao file
-//				String name = String.format("%s%s", RandomStringUtils.randomAlphanumeric(10), mpf.getOriginalFilename());
-//				File file = new File(pathSaveFile + name);
-				File file = new File(pathSaveFile + mpf.getOriginalFilename());
+//				B1: lay ra duong dan se luu file
+				pathSaveFile.append("/image/");
+				
+//				pathSaveFile = "/com/comaymanagement/cmd/image/";
+				// Get extension
+				String[] extensions =  mpf.getOriginalFilename().split("\\.");
+				StringBuilder ext = new StringBuilder(".").append(extensions[extensions.length-1]);
+				String name = String.format("%s_%s",new SimpleDateFormat("yyyy-MM-dd-HHmmss").format(new Date().getTime()),RandomStringUtils.randomAlphanumeric(5)+ext);
+//				B2: Tao file
+				File file = new File(pathSaveFile + name);
+//				File file = new File(pathSaveFile + mpf.getOriginalFilename());
+				System.out.println("Path save file: " + pathSaveFile);
 //			B3: dung ham trong thu vien commmon de save
 				try {
 					mpf.transferTo(file);
+					return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("OK", "", name));
 				} catch (IOException e) {
-					System.err.println(e.getStackTrace());
+					System.err.println(e.getMessage());
+					return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("OK",e.getMessage(), mpf.getOriginalFilename()));
 				}
 			}
+			return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("OK","No image to save", ""));
 			
-			System.out.println(pathSaveFile);
-			return "upload thanh cong";
 	}
+	@GetMapping(value = "/get-image/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImageWithMediaType(
+    		@PathVariable String name
+    		) throws IOException {
+		StringBuilder baseURL = new StringBuilder(System.getProperty("user.dir")).append("/image/");
+//		File image = new File(baseURL + name.trim());
+		final InputStream in = new BufferedInputStream(new FileInputStream(baseURL + name.trim())); 
+//        final InputStream in = getClass().getResourceAsStream(baseURL + name.trim());
+        return IOUtils.toByteArray(in);
+    }
 }
